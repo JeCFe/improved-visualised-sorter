1d7870edd390aa98206c6dcd5349d2db2bb04372
Jessica Fealy <jessicaf091@outlook.com>
2024-01-15 23:10:16 +0000

    bubble sort

---
+++ sorting-algorithms/bubble-sort.ts
@@ -0,0 +1,25 @@
+import { Bars } from "@/app/page";
+import { swap } from "@/helpers";
+import { Animations } from "@/types";
+
+export const getBubbleSort = (array: Bars[]) => {
+  const animations: Animations = [];
+  const n = array.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j].number > array[j + 1].number) {
+        animations.push([j, array[j + 1].number, true]);
+        animations.push([j, array[j + 1].number, false]);
+        animations.push([j + 1, array[j].number, true]);
+        animations.push([j + 1, array[j].number, false]);
+        swap(j, j + 1, array);
+      } else {
+        if (j != 0) {
+          animations.push([j - 1, array[j - 1].number, true]);
+          animations.push([j - 1, array[j - 1].number, false]);
+        }
+      }
+    }
+  }
+  return animations;
+};
